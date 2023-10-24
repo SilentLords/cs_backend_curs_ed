6da57d6e99ad4347fe60a7450635265f131969b5
SilentLords <41440891+SilentLords@users.noreply.github.com>
2023-10-24 23:27:43 +0500

    #Create User, Billing models, #Create DuplicatedEntryError exception #Update postgresql.py Add Async support

---
+++ app/internal/models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, Float
+from app.pkg.postgresql import Base
+
+
+class User(Base):
+    __tablename__: str = 'users'
+
+    id: Column = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    nickname: Column = Column(String, unique=True)
+    ethereum_ID: Column = Column(String, unique=True)
+    billing_model: Column = Column(Integer, ForeignKey("billing.id"))
+
+
+class Billing(Base):
+    __tablename__: str = "billing"
+    id: Column = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    banned: Column = Column(Boolean, default=True)
+    money_count: Column = Column(Float, default=0)
+    owner: Column = Column("User", back_populates="items")
--- app/pkg/postgresql.py
+++ app/pkg/postgresql.py
@@ -1,19 +1,30 @@
-import databases
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
+
 from app.configuration.settings import Settings
 
 settings = Settings()
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password}@db/{settings.db_name}"
 
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Model = declarative_base()
-database = databases.Database(SQLALCHEMY_DATABASE_URL)
-#test
+Base = declarative_base()
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+# Dependency
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session

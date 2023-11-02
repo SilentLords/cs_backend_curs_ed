367c0073a260d1c74529782568460157b3259fe1
SEnderKLigan <mr.niki002@gmail.com>
2023-11-02 14:04:53 +0500

    #Create test auth

--- app/internal/routes/users.py
+++ app/internal/routes/users.py
@@ -31,7 +31,7 @@ async def login(request: Request):
 
 @router.get("/login/callback")
 async def auth(request: Request):
-    print('req:', request)
+    print('req:', request.json())
 
     try:
         token = await oauth.create_client("Client_cs2").authorize_access_token(request)

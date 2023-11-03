45290d9aae580a8f03af6f41df57cca9f88b5f1d
SEnderKLigan <mr.niki002@gmail.com>
2023-11-03 21:23:43 +0500

    #ADD DECLARATIVE_BASE integrations ...

--- app/internal/models/user.py
+++ app/internal/models/user.py
@@ -1,10 +1,14 @@
 from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, Float, MetaData, Table
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
+
+
 class User(Base):
     __tablename__: str = 'users'
 
     id: Column = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    openid: Column = Column(String, unique=True)
     nickname: Column = Column(String, unique=True)
     ethereum_ID: Column = Column(String, unique=True)
     billing_model: Column = Column(Integer, ForeignKey("billing.id"))

992d6cc929356c4b23d8618774599ebf48a2dea3
SEnderKLigan <mr.niki002@gmail.com>
2023-11-03 05:05:57 +0500

    #Create test auth

--- main.py
+++ main.py
@@ -4,14 +4,35 @@ import typer
 
 from app.pkg.postgresql import init_models
 
-cli = typer.Typer()
+import typer
+
+app = typer.Typer()
 
 
-@cli.command()
-def db_init_models():
-    asyncio.run(init_models())
-    print("Done")
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}")
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
 
 
 if __name__ == "__main__":
-    cli()
+    app()
+
+# cli = typer.Typer()
+#
+#
+# @cli.command()
+# def db_init_models():
+#     asyncio.run(init_models())
+#     print("Done")
+#
+#
+# if __name__ == "__main__":
+#     cli()

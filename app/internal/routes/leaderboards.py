f2a6da6ec15573c251661366b6c4838baef518ee
SEnderKLigan <mr.niki002@gmail.com>
2023-11-03 23:08:36 +0500

    #ADD DECLARATIVE_BASE integrations ...

--- app/internal/routes/leaderboards.py
+++ app/internal/routes/leaderboards.py
@@ -8,7 +8,8 @@ router = APIRouter(
 
 async def fetch_data_from_external_api(q_param: dict, headers: dict):
     async with httpx.AsyncClient() as client:
-        response = await client.get("https://open.faceit.com/data/v4/leaderboards/651da31af3e96d2044a35366", params=q_param, headers=headers)
+        response = await client.get("https://open.faceit.com/data/v4/leaderboards/651da31af3e96d2044a35366",
+                                    params=q_param, headers=headers)
 
         if response.status_code == 200:
             return response.json()
@@ -21,3 +22,4 @@ async def get_leaderboard(offset: int, limit: int = 20):
     api_key = "2a4e3640-5b2f-41a8-9be4-756476cfa0d6"
     q_param = {'offset': offset, "limit": limit}
     headers = {'accept': 'application/json', 'Authorization': f'Bearer {api_key}'}
+    return await fetch_data_from_external_api(q_param=q_param, headers=headers)

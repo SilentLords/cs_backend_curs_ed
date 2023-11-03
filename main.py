11bf28987838675a85b591b016555471b8622e66
SEnderKLigan <mr.niki002@gmail.com>
2023-11-03 05:07:08 +0500

    #Create test auth

--- main.py
+++ main.py
@@ -8,18 +8,22 @@ import typer
 
 app = typer.Typer()
 
-
+#
+# @app.command()
+# def hello(name: str):
+#     print(f"Hello {name}")
+#
+#
+# @app.command()
+# def goodbye(name: str, formal: bool = False):
+#     if formal:
+#         print(f"Goodbye Ms. {name}. Have a good day.")
+#     else:
+#         print(f"Bye {name}!")
 @app.command()
-def hello(name: str):
-    print(f"Hello {name}")
-
-
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
+def db_init_models():
+    asyncio.run(init_models())
+    print("Done")
 
 
 if __name__ == "__main__":

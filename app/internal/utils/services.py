685b52ac324dc2fc566ca539d47b9cef57c547c2
SEnderKLigan <mr.niki002@gmail.com>
2023-11-04 00:05:11 +0500

    #ADD DECLARATIVE_BASE integrations ...

--- app/internal/utils/services.py
+++ app/internal/utils/services.py
@@ -62,7 +62,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(nickname=token_data.username)
+    user = await get_user(nickname=token_data.username)
     if user is None:
         raise credentials_exception
     return user

424a39274a95c6908c5615a41396de09770f99a6
SEnderKLigan <mr.niki002@gmail.com>
2023-11-07 04:06:00 +0500

    #ADD DECLARATIVE_BASE integrations ...

--- app/configuration/settings.py
+++ app/configuration/settings.py
@@ -16,8 +16,8 @@ class Settings(BaseSettings):
     oauth_client_secret: str = "9ne2aL8JR1T8htrwMzKFmvt6FHnGTQPlHok6ytz4"
     # oauth_authorize_redirect_path: str = Field(..., env='OAUTH_AUTHORIZE_REDIRECT_PATH')
     oauth_authorize_redirect_path: str = "https://cs2-backend.evom.dev/api/v1/users/login/callback"
-    faceit_api_key = "2a4e3640-5b2f-41a8-9be4-756476cfa0d6"
-    leaderboard_id = "651da31af3e96d2044a35366"
+    faceit_api_key:str = "2a4e3640-5b2f-41a8-9be4-756476cfa0d6"
+    leaderboard_id:str = "651da31af3e96d2044a35366"
     # JWT
     jwt_secret_key: str = "9f63d3214da92c45a40d3866a5fb93e998afca753a557fee0de7ba8d6551767c"
     jwt_algorithm: str = 'HS256'

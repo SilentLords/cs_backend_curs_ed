a6cf08d6c54371f29cb956fb44606adbf2520765
Danirill <45917937+Danirill@users.noreply.github.com>
2023-11-14 21:55:56 +0500

    Update postgresql.py

--- app/pkg/postgresql.py
+++ app/pkg/postgresql.py
@@ -7,8 +7,9 @@ from app.configuration.settings import Settings
 
 settings = Settings()
 ca_path = "app/certs/ca-certificate.crt"
-ssl_args = {'ssl_ca': ca_path}
-SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{settings.db_username}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+# ssl_args = {'ssl_ca': ca_path}
+ssl_args = {'ssl': {'ca': ca_path}}
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{settings.db_username}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}?sslmode=require"
 
 if settings.is_prod == "true":
     engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, connect_args=ssl_args)

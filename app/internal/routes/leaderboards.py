721e6a419aa44beac95364b6f0c18d6b8d19840c
SEnderKLigan <mr.niki002@gmail.com>
2023-11-22 19:48:54 +0500

    Merge leaderboard methods

--- app/internal/routes/leaderboards.py
+++ app/internal/routes/leaderboards.py
@@ -10,10 +10,17 @@ router = APIRouter(
 
 
 @router.get('/')
-async def get_leaderboard(offset: int, limit: int = 20):
+async def get_leaderboard(offset: int, limit: int = 20, get_latest: bool = False):
+    leaderboard_id: str = settings.leaderboard_id
     q_param = {'offset': offset, "limit": limit}
-    data = await fetch_data_from_external_api(q_param=q_param, path=f'leaderboards/{settings.leaderboard_id}')
-    print(data)
+    if get_lastest_leaderboard:
+        leaderboards_data = await fetch_data_from_external_api(q_param=q_param, path=f'leaderboards/hubs/8a9629cf-c837-4389-97a1-1c47cf886df4')
+        if leaderboards_data:
+            leaderboard_id = max(leaderboards_data['items'], key=lambda x:x['start_date'])['leaderboard_id']
+        else:
+            return {"message": "Auth Error"}
+
+    data = await fetch_data_from_external_api(q_param=q_param, path=f'leaderboards/{leaderboard_id}')
     if data:
         new_items = []
         for item in data['items']:
